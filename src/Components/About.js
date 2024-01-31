2a5bffef9387c59c10e8c368922f859c0bfa74b9
pranshu baghel <devloperpranshu@gmail.com>
2024-01-31 10:43:48 +0530

    first react app

---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <>
+        <h1>Why is react is so popular </h1>
+        </>
+    )
+}
+ 
+export default About;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Hello React js</h1>
+
+        </>
+
+    )
+
+}
+
+export default Home;

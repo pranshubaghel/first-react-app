e73b3095385e96fea0b8b3e441f6be9833d014ed
pranshu baghel <devloperpranshu@gmail.com>
2024-02-20 20:47:00 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 
 import ColorPicker from './Components/Colorpicker';
+import User from './Components/User';
+
 
 
 function App() {
   return (
 
 <>
-
-
 <ColorPicker/>
+<User name="Pranshu"
+ adress="Rewa"/>
 </>
 
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/Components/User.jsx
@@ -0,0 +1,9 @@
+const User = (props) => {
+  return (
+    <div>
+      <p>User Name: {props.name}</p>
+      <h2>Adress: {props.adress}</h2>
+    </div>
+  );
+};
+export default User;
